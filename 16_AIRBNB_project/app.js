575417a9cf49d18522289d8138093712652658e3
shiva gupta <shivagupta5607@gmail.com>
2024-07-18 20:31:41 +0530

    complete UI of project using bootstrap

--- 16_AIRBNB_project/app.js
+++ 16_AIRBNB_project/app.js
@@ -4,13 +4,17 @@ const Listing = require('./models/listing.js');
 const path = require('path');
 const { send } = require('process');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
+
 const app = express();
 
 
 app.set('view engine', 'ejs');
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.engine('ejs', ejsMate);
 
 const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
 async function main() {
@@ -61,7 +65,6 @@ app.get('/listings/new', (req, res) => {
 app.get('/listings/:id', async (req, res) => {
     let { id } = req.params;
     let listing = await Listing.findById(id);
-
     res.render('listings/show.ejs', { listing });
 })
 
