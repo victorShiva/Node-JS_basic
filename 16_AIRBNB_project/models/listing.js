63ee31cfb9c68bbbadc625bc2670428e9bdd784b
shiva gupta <shivagupta5607@gmail.com>
2024-07-21 13:42:22 +0530

    Set the form validation using bootstrap

--- 16_AIRBNB_project/models/listing.js
+++ 16_AIRBNB_project/models/listing.js
@@ -15,7 +15,10 @@ const listingSchema = new Schema({
                 ? "https://images.unsplash.com/photo-1720878986927-9ea5fb0ebdf9?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 : v,
     },
-    price: Number,
+    price: {
+        type: Number,
+        required: true,
+    },
     location: String,
     country: String
 });
